95ce7805006249a10664eb4d59746378f544bdfb
Dingchao Zhang <ks692@cummins.com>
2015-09-22 15:39:15 -0400

    finished twoSum problem

---
+++ Problem 01 - Two Sum/TwoSum.py
@@ -0,0 +1,8 @@
+def twoSum(self,nums,target):
+        target = raw_input("Please enter a target number")
+        nums = [1,2,3,4,5,6,7,8,9,10]
+        print target
+        print nums
+
+        print self
+twoSum()
